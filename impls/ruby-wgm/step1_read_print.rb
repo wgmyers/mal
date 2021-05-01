2678f3831d03fe1598332712d0e9bacebd445421
Wayne Myers <wgmyers@gmail.com>
2021-05-01 02:50:24 +0100

    Add files ready to implement Step 1

---
+++ impls/ruby-wgm/step1_read_print.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+# step0_repl.rb
+# In which we implement the basic skeleton of the interpreter
+
+require_relative 'readline'
+
+# In this step, READ, EVAL and PRINT just return their arguments
+def READ(arg)
+  return arg
+end
+def EVAL(arg)
+  return arg
+end
+def PRINT(arg)
+  return arg
+end
+
+# Here, rep just passes arg to READ, EVAL and PRINT in order and returns the result
+def rep(arg)
+  arg = READ(arg)
+  arg = EVAL(arg)
+  arg = PRINT(arg)
+  return arg
+end
+
+# Print a prompt and take input
+# If input is EOF then stop
+# Otherwise pass input through rep and print it
+def main()
+  prompt = "user> "
+  loop do
+    line = grabline(prompt)
+    # The readline library returns nil on EOF
+    if line == nil
+      break
+    end
+    puts rep(line)
+  end
+end
+
+main
