89cc124cf6060e808620a3c4205cf944df965c16
Wayne Myers <wgmyers@gmail.com>
2021-05-02 06:45:25 +0100

    Prepare for step 2

---
+++ impls/ruby-wgm/step2_eval.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/env ruby
+
+# step0_repl.rb
+# In which we implement the basic skeleton of the interpreter
+
+require_relative 'readline'
+require_relative 'reader'
+require_relative 'printer'
+
+# In earlier steps READ, EVAL and PRINT just return their arguments
+# But now we want to catch errors from read_str and pass them back to
+# our main loop. Later, presumably, we catch errors from EVAL also, and
+# for completeness, from PRINT.
+def READ(arg)
+  begin
+    return read_str(arg)
+  rescue => e
+    raise e
+  end
+end
+def EVAL(arg)
+  return arg
+end
+def PRINT(arg)
+  return pr_str(arg)
+end
+
+# Here, rep just passes arg to READ, EVAL and PRINT in order and returns the result
+def rep(arg)
+  begin
+    arg = READ(arg)
+  rescue => e
+    raise e
+  end
+  arg = EVAL(arg)
+  arg = PRINT(arg)
+  return arg
+end
+
+# Print a prompt and take input
+# If input is EOF then stop
+# Otherwise pass input through rep and print it
+def main()
+  prompt = "user> "
+  loop do
+    line = grabline(prompt)
+    # The readline library returns nil on EOF
+    if line == nil
+      break
+    end
+    begin
+      puts rep(line)
+    rescue => e
+      puts "Error: " + e.message
+    end
+  end
+end
+
+main
