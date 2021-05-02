5fbaa6ca82e8a1aa488e6caab923fc9876597c48
Wayne Myers <wgmyers@gmail.com>
2021-05-02 07:13:07 +0100

    Rearrange errors in order

--- impls/ruby-wgm/errors.rb
+++ impls/ruby-wgm/errors.rb
@@ -3,6 +3,12 @@
 # Custom exception classes for errors.
 # NB - Inherit from StandardError and not Exception or things don't work.
 
+class MalBadHashMapError < StandardError
+  def initialize(msg = "Number of keys does not match number of values in hashmap")
+    super(msg)
+  end
+end
+
 class MalMismatchParensError < StandardError
   def initialize(msg = "Mismatched parentheses at EOF")
     super(msg)
@@ -15,20 +21,14 @@ class MalMismatchQuotesError < StandardError
   end
 end
 
-class MalBadHashMapError < StandardError
-  def initialize(msg = "Number of keys does not match number of values in hashmap")
+class MalNestedWithMetaError < StandardError
+  def initialize(msg = "Nested ^/with-meta macros not supported")
     super(msg)
   end
 end
 
 class MalUnknownListTypeError < StandardError
   def initialize(msg = "Unknown list type before EOF")
-    super(msg)
-  end
-end
-
-class MalNestedWithMetaError < StandardError
-  def initialize(msg = "Nested ^/with-meta macros not supported")
     super(msg)
   end
 end
