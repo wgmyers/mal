fe90a75bc5962c49162f6927d7ffc48d2eb7bbc3
Wayne Myers <wgmyers@gmail.com>
2021-05-03 13:26:44 +0100

    Initial implementation of Step 4 core functions

--- impls/ruby-wgm/core.rb
+++ impls/ruby-wgm/core.rb
@@ -8,12 +8,23 @@ module MalCore
     '*'      => lambda { |x,y| MalNumber.new(x.data * y.data) },
     '/'      => lambda { |x,y| MalNumber.new(x.data / y.data) }, # NB Divide by zero caught by Ruby, not us
     'prn'    => lambda { |x| puts(pr_str(x, true))
-                            return MalNil.new() },
+                             return MalNil.new()
+                       },
     'list'   => lambda { |*x| l = MalList.new()
                               x.each { |i| l.push(i) }
-                              return l },
-    'list?'  => lambda { |x| x.is_a?(MalList) ? MalTrue.new() : MalFalse.new() }, # FIXME Breaks if list not empty.
+                              return l
+                       },
+    'list?'  => lambda { |x| x.is_a?(MalList) ? MalTrue.new() : MalFalse.new() },
     'empty?' => lambda { |x| x.data.length == 0 ? MalTrue.new() : MalFalse.new() },
-    '='      => lambda { |x,y| x == y }                             # FIXME - doesn't check type or handle lists
+    'count'  => lambda { |x| x.data.length },
+    '='      => lambda { |x,y| if (x.class != y.class) || (x.data != y.data)
+                                 return MalFalse.new()
+                               end
+                               return MalTrue.new()
+                       }, # FIXME does not work on lists :(
+    '<'      => lambda { |x,y| x.data < y.data ? MalTrue.new() : MalFalse.new() },
+    '<='     => lambda { |x,y| x.data <= y.data ? MalTrue.new() : MalFalse.new() },
+    '>'      => lambda { |x,y| x.data > y.data ? MalTrue.new() : MalFalse.new() },
+    '>='     => lambda { |x,y| x.data >= y.data ? MalTrue.new() : MalFalse.new() },
   }
 end
