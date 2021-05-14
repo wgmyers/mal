56c0cce87d7f09af18e5dd41df9b1b65a0f659ed
Wayne Myers <wgmyers@gmail.com>
2021-05-15 00:09:31 +0100

    Fix rubocop layout complaints

--- impls/ruby-wgm/core.rb
+++ impls/ruby-wgm/core.rb
@@ -282,9 +282,12 @@ module MalCore
   }
 
   Mal = {
-    'not' => '(def! not (fn* (a) (if a false true)))',
+    'not'       => '(def! not (fn* (a) (if a false true)))',
     'load-file' => '(def! load-file (fn* (f) (eval (read-string (str "(do " (slurp f) "\nnil)")))))',
-    'cond' => "(defmacro! cond (fn* (& xs) (if (> (count xs) 0) (list \'if (first xs) (if (> (count xs) 1) (nth xs 1) (throw \"odd number of forms to cond\")) (cons \'cond (rest (rest xs)))))))"
+    'cond'      => "(defmacro! cond (fn* (& xs)
+                       (if (> (count xs) 0)
+                         (list \'if (first xs) (if (> (count xs) 1) (nth xs 1) (throw \"odd number of forms to cond\"))
+                         (cons \'cond (rest (rest xs)))))))"
   }
 
   # Evaller
