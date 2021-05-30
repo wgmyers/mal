f7c10f30a9955a5c1e01a65e341e6dc8f1b30626
Wayne Myers <wgmyers@gmail.com>
2021-05-30 02:08:37 +0100

    Trim trailing whitespace from output

--- impls/cobol/step0_repl.cob
+++ impls/cobol/step0_repl.cob
@@ -1,3 +1,6 @@
+*> step0_repl.cob
+*> NB We are using the modern 'free' format, so must compile with -free
+*>    This allows us not to have to muck about with weird columns
 IDENTIFICATION DIVISION.
 PROGRAM-ID. MAL-STEP0.
 AUTHOR. WAYNE MYERS.
@@ -6,7 +9,7 @@ DATA DIVISION.
 WORKING-STORAGE SECTION.
 01 WS-PROMPT-MSG PIC X(6) VALUE 'user> '.
 01 WS-QUIT PIC 9(1) VALUE 0.
-01 WS-INPUT PIC X(1).
+01 WS-INPUT PIC X(255).
 
 PROCEDURE DIVISION.
 *> Main program loop
@@ -14,12 +17,21 @@ PROCEDURE DIVISION.
 STOP RUN.
 
 REPL-PARA.
-*> Perform the READ/EVAL/PRINT loop
-     PERFORM RESPONSE-PARA.
+*> Implement the READ/EVAL/PRINT loop
+     PERFORM READ-PARA.
+     PERFORM EVAL-PARA.
+     PERFORM PRINT-PARA.
 
-RESPONSE-PARA.
-*> Utility function to get response from user
+READ-PARA.
+*> Display prompt and get response from user
      DISPLAY WS-PROMPT-MSG WITH NO ADVANCING.
      ACCEPT WS-INPUT.
      IF WS-INPUT = "q" OR WS-INPUT = "Q" THEN
        MOVE 1 TO WS-QUIT.
+
+EVAL-PARA.
+*> Do nothing for now.
+
+PRINT-PARA.
+*> Print respoonse from user
+     DISPLAY FUNCTION TRIM(WS-INPUT).
